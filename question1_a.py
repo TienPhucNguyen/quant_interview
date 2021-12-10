7ec3ab0cf90afe8fb667ec7ec391b0cb49e88585
TienPhucNguyen <86545683+TienPhucNguyen@users.noreply.github.com>
2021-12-10 05:36:27 +0100

    Add files via upload
    These are answers of questions 1 and 3. Question 3 is answered in 2 ways.

---
+++ question1_a.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+data = pd.DataFrame({
+    'id': [1, 2, 3, 4, 5, 6, 7],
+    'Name': ['John', 'Mike', 'Sally', 'Jane', 'Joe', 'Dan', 'Phil'],
+    'Salary': [300, 200, 550, 500, 600, 600, 550],
+    'manager_id': [3, 3, 4, 7, 7, 3, 'Null']
+})
+
+result = []
+for i in range(len(data)):
+    for j in range(len(data)):
+        if data['manager_id'][i] == data['id'][j] and data['Salary'][i] > data['Salary'][j]:
+            result.append(data['Name'][i])
+
+print('People whose salaries are greater than their managers: ', result)
+
+
+sum_salaries = 0
+number_of_people = 0
+
+for i in range(len(data)):
+    if data['id'][i] not in list(data['manager_id']):
+        sum_salaries += data['Salary'][i]
+        number_of_people += 1
+
+average_salary = sum_salaries / number_of_people
+print('The average salary of employees who do not manage anyone is: ', average_salary)
